0c7067e643f266593c690e939c478d75664d0073
Matthew Einhorn <moiein2000@gmail.com>
2019-10-24 23:05:27 -0400

    Switch to github actions.

--- setup.py
+++ setup.py
@@ -1,22 +1,11 @@
-try:
-    from setuptools import setup, Extension
-except ImportError:
-    from distutils.core import setup
-    from distutils.extension import Extension
-import os
+from setuptools import setup, Extension
 import sys
 from os.path import join, exists, dirname, abspath, isdir
 from os import environ, listdir
 from pyflycap2 import __version__
-try:
-    import Cython.Compiler.Options
-    #Cython.Compiler.Options.annotate = True
-    from Cython.Distutils import build_ext
-    have_cython = True
-    cmdclass = {'build_ext': build_ext}
-except ImportError:
-    have_cython = False
-    cmdclass = {}
+from Cython.Distutils import build_ext
+cmdclass = {'build_ext': build_ext}
+
 
 def get_wheel_data():
     data = []
@@ -29,12 +18,15 @@ def get_wheel_data():
         )
     return data
 
-if sys.platform in ('win32', 'cygwin'):
-    libraries = ['FlyCapture2_C_v110', 'FlyCapture2GUI_C_v110']
-else:
-    libraries = ['flycapturegui-c', 'flycapture-c']
+
 include_dirs = []
 library_dirs = []
+
+if sys.platform in ('win32', 'cygwin'):
+    libraries = ['FlyCapture2_C_v140', 'FlyCapture2GUI_C_v140']
+else:
+    libraries = ['flycapturegui-c', 'flycapture-c']
+    include_dirs.append('/usr/include/flycapture')
 
 include = environ.get('PYFLYCAP2_INCLUDE')
 if include:
@@ -46,15 +38,13 @@ if lib:
 
 
 mods = ['interface']
-extra_compile_args = ["-O3", '-fno-strict-aliasing']
-mod_suffix = '.pyx' if have_cython else '.c'
+mod_suffix = '.pyx'
 include_dirs.append(join(abspath(dirname(__file__)), 'pyflycap2', 'includes'))
 
 ext_modules = [Extension(
     'pyflycap2.' + src_file,
     sources=[join('pyflycap2', src_file + mod_suffix)], libraries=libraries,
-    include_dirs=include_dirs, library_dirs=library_dirs,
-    extra_compile_args=extra_compile_args) for src_file in mods]
+    include_dirs=include_dirs, library_dirs=library_dirs) for src_file in mods]
 
 for e in ext_modules:
     e.cython_directives = {"embedsignature": True}
@@ -74,10 +64,9 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Topic :: Multimedia :: Graphics :: Capture :: Digital Camera',
         'Topic :: Multimedia :: Graphics :: Capture',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Operating System :: Microsoft :: Windows',
         'Operating System :: POSIX :: Linux',
         'Intended Audience :: Developers'],
